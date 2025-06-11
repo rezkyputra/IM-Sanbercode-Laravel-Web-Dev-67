a8a111b57244c8170a55fc256d781664d2d4cbba
rezkyputra <rezky.putra46@gmail.com>
2025-06-11 17:31:07 +0800

    latihan templating

--- portalNews/resources/views/welcome.blade.php
+++ portalNews/resources/views/welcome.blade.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Utama</h1>
-    <ul>
-        <li><a href="#">link 1</a></li>
-        <li>List 2 <a href="/pendaftaran">Pendaftarn</a></li>
-        <li><a href="#">link 3</a></li>
-    </ul>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title')
+    Halaman Utama
+@endsection
+@section('content')
+    
+<ul>
+    <li><a href="#">link 1</a></li>
+    <li>List 2 <a href="/pendaftaran">Pendaftarn</a></li>
+    <li><a href="#">link 3</a></li>
+</ul>
+@endsection
